904a42fbc45a175542e057e2ad89669bb8568720
ddigges <deborahgertrude_digges@intuit.com>
2016-12-16 17:29:16 +0530

    Wire simulator to work with simple model(ends up in the ocean again)

--- P3-Behaviour-Cloning/drive.py
+++ P3-Behaviour-Cloning/drive.py
@@ -2,6 +2,7 @@ import argparse
 import base64
 import json
 
+import cv2
 import numpy as np
 import socketio
 import eventlet
@@ -25,6 +26,28 @@ app = Flask(__name__)
 model = None
 prev_image_array = None
 
+def pre_process(image):
+    """
+        image: input image
+        
+        - convert to grayscale
+        - resize image to half it's original size
+    """
+    
+    image = image.reshape((image.shape[1], image.shape[2], image.shape[3]))
+    image = np.mean(image, axis=2)
+    data_min = np.min(image)
+    data_max = np.max(image)
+    b = 0.5
+    a = -0.5
+    
+    image = ((b-a) * (image - data_min)/(data_max - data_min)) + a
+    return cv2.resize(image, (0,0), fx=0.5, fy=0.5) 
+
+def flatten(image):
+    
+    return image.reshape(-1, image.shape[0] * image.shape[1])
+
 @sio.on('telemetry')
 def telemetry(sid, data):
     # The current steering angle of the car
@@ -38,9 +61,11 @@ def telemetry(sid, data):
     image = Image.open(BytesIO(base64.b64decode(imgString)))
     image_array = np.asarray(image)
     transformed_image_array = image_array[None, :, :, :]
+    transformed_image_array = flatten(pre_process(transformed_image_array))
+    
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
-    #steering_angle = float(model.predict(transformed_image_array, batch_size=1))
-    steeting_angle = 0.0
+    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+    
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
     throttle = 0.2
     print(steering_angle, throttle)
@@ -66,14 +91,14 @@ if __name__ == '__main__':
     help='Path to model definition json. Model weights should be on the same path.')
     args = parser.parse_args()
     
-    """
+    
     with open(args.model, 'r') as jfile:
-        model = model_from_json(json.load(jfile))
+        model = model_from_json(jfile.read())
 
     model.compile("adam", "mse")
     weights_file = args.model.replace('json', 'h5')
     model.load_weights(weights_file)
-    """
+    
 
     # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
