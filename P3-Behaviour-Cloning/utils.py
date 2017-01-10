4a5582a4dba49e10e99ce85e203c602e7d388d39
ddigges <deborahgertrude_digges@intuit.com>
2017-01-10 21:05:54 +0530

    Add latest utils.py

--- P3-Behaviour-Cloning/utils.py
+++ P3-Behaviour-Cloning/utils.py
@@ -10,4 +10,4 @@ def pre_process(image, top_prop=0.35, bottom_prop=0.1):
     rows_to_crop_bottom = int(image.shape[0] * 0.1)
     image = image[rows_to_crop_top:image.shape[0] - rows_to_crop_bottom, :]    
 
-    return cv2.resize(image, (200, 66))
+    return cv2.resize(image, (0,0), fx=0.5, fy=0.5)
